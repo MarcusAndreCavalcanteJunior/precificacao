062c649f147d33eba1f629f9aebe1990fdcfde0d
André <andrecjuniorr@gmai.com>
2026-06-25 14:37:07 -0300

    05.28. Métodos que alteram o valor de parâmetro do tipo primitivo

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Principal {
+
+    public static void main(String[] args) {
+        double precoCompraFornecedor = 140;
+
+        ServicoDePrecificacao servicoDePrecificacao = new ServicoDePrecificacao();
+        servicoDePrecificacao.calcularPrecoVenda(precoCompraFornecedor);
+
+        System.out.printf("Preço: %.2f%n", precoCompraFornecedor);
+    }
 }
--- src/ServicoDePrecificacao.java
+++ src/ServicoDePrecificacao.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class ServicoDePrecificacao {
+/*NÃO É UMA BOA PRÁTICA VC FICAR ALTERANDO O VALOR DE PARAMETRO, O IDEAL
+* CASO VC PRECISE ALTERAR O VALOR DE UMA VARIAVEL É QUE VC ECLARE UMA NOVA
+* VARIAVEL COMO IREI COMENTAR NO METODO ESCRITO */
+    void calcularPrecoVenda (double precoCusto) {
+        // adciona 20% de margem de lucro
+        precoCusto = precoCusto * 1.20;
+
+        // faz varias outras coisas
+        /*JEITO CERTO SERIA MasNÃO É UMA BOA PRÁTICA FICAR ALTERANDO O
+        VALOR DA VARIAVEL QUE VC RECEBEU COMO PARAMETRO
+        double calcularPrecoVenda (double precoCusto) {
+        * double precoVenda = precoCusto * 1.20
+        * return precoVenda
+        }*/
+    }
+
+
 }
+
+
+/* Eu não sabia que da para listar todos os TODO implemetar no caso o que
+fazer a tradução e para achar eles é ir nos nos 3 pontinhos e ele vai e
+listar todos para vc*/
